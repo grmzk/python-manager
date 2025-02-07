a499b3aa63ee0f99afb3fdf64bd13c523333e6cb
Igor Muzyka <muzyka-iv@yandex.ru>
2025-02-07 14:03:30 +0600

    added version info for system python3 package

--- actions/utils.py
+++ actions/utils.py
@@ -8,4 +8,11 @@ def get_python_exec_files() -> list:
     )
     python_exec_files = find_python_out.stdout.splitlines()
     python_exec_files.sort(key=lambda file: int(file.split('python3.')[1]))
+    find_system_python_out = run(
+        r'find /usr/bin -regex ^/usr/bin/python3$'.split(),
+        stdout=PIPE, stderr=PIPE, encoding='utf-8'
+    )
+    if find_system_python_out.stdout:
+        python_exec_files = ([find_system_python_out.stdout.splitlines()[0]]
+                             + python_exec_files)
     return python_exec_files
